c4736e9df4881788f9a2fb95aa478b7fe13dd7d1
Lagg333673034 <144306091+Lagg333673034@users.noreply.github.com>
2025-05-05 16:31:29 +0400

    123

--- doJob.js
+++ doJob.js
@@ -3,7 +3,7 @@ const https = require('https');
 
 //https://crontab.guru/#*/10_*_*_*_*
 
-const doJob = new cron.CronJob('*/10 * * * *', function(){
+const doJob = new cron.CronJob('*/3 * * * *', function(){
     console.log('not sleep');
 
     const url = "https://lagg333673034-my-test-app.netlify.app";
