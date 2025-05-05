be848122a8b322f865ffd607b881cc9b67297229
Lagg333673034 <144306091+Lagg333673034@users.noreply.github.com>
2025-05-05 16:46:26 +0400

    31

--- models/userModel.js
+++ models/userModel.js
@@ -14,6 +14,8 @@ class User{
         return result[0][0];
     }
     static async findByEmail(email){
+        console.log("findByEmail==");
+        console.log(email);
         let sql = `SELECT 
         id, email, password, entryMethod, 
         CASE 
