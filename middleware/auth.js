e3bd78da32d519afd3b8c0b9ff649a61d20ca395
Lagg333673034 <144306091+Lagg333673034@users.noreply.github.com>
2025-05-05 12:29:46 +0400

    123

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,38 +8,20 @@ module.exports = (req, res, next) => {
     }
 
     try{
-        //console.log('req.headers.authorization=');
-        //console.log(req.headers.authorization);
-
         const authorizationHeader = req.headers.authorization;
         if(!authorizationHeader){
-            //return res.status(401).json({message:"Auth error.1"})
             throw ErrorController.UnAuthorizedError();
         }
-
-
         const accessToken = authorizationHeader.split(' ')[1];
         if(!accessToken){
-            //return res.status(401).json({message:"Auth error.2"})
             throw ErrorController.UnAuthorizedError();
         }
-
-
-        //const userData = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
         const userData = jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET_KEY);
         if(!userData){
-            //return res.status(401).json({message:"Auth error.3"})
             throw ErrorController.UnAuthorizedError();
         }
 
-        //console.log('userData=');
-        //console.log(userData);
-
         req.fromJWT = userData;
-        //req.fromJWT = "";
-        
-        //console.log('req.fromJWT=');
-        //console.log(req.fromJWT);
 
         next()
     }catch(error){
