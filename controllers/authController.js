8ba4c60040ee3bb05db442679e496b53a720689e
Lagg333673034 <144306091+Lagg333673034@users.noreply.github.com>
2026-05-28 19:29:18 +0400

    2342

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,7 @@ const transporter = nodemailer.createTransport({
     port: Number(process.env.MAIL_PORT),
 
     secure: false,
+    requireTLS: true,
 
     auth:{
         user: process.env.MAIL_USER,
